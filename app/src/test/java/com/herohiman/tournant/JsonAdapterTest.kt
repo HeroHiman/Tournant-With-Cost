5cd08439af76cdf6f1013217a511f294552ef112
HeroHiman <jainhimanshu952@gmail.com>
2026-09-22 17:25:14 +0000

    Fix JsonAdapterTest to use classpath resource loading and move test file to test/resources

--- app/src/test/java/com/herohiman/tournant/JsonAdapterTest.kt
+++ app/src/test/java/com/herohiman/tournant/JsonAdapterTest.kt
@@ -4,14 +4,16 @@ import com.herohiman.tournant.data.Cookbook
 import com.herohiman.tournant.utils.RecipeJsonAdapter
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
-import java.io.File
+import java.io.FileNotFoundException
 
 class JsonAdapterTest {
 
 	@Test
 	fun jsonTest() {
 		val json = RecipeJsonAdapter.adapter.toJson(Cookbook(sampleRecipeWithIngredients(1)))
-		val expected = File("src/test/java/com.herohiman.tournant/JsonAdapterTestOutput").readText()
+		val inputStream = javaClass.classLoader.getResourceAsStream("JsonAdapterTestOutput")
+			?: throw FileNotFoundException("Could not find JsonAdapterTestOutput in test resources")
+		val expected = inputStream.bufferedReader().use { it.readText() }
 		assertEquals(expected, json)
 	}
 
